86ff856b4fc8615c9ce5866a4d1ff1d3dba1b999
Dika Wicaksono <dhkwck00@gmail.com>
2024-04-04 02:32:22 +0700

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import store from "./store.jsx";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Layout from "./components/Layout.jsx";
+import Private from "./private/private.jsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Login />} />
+      <Route path="" element={<Layout />}>
+        <Route path="dashboard" element={<Dashboard />}></Route>
+      </Route>
+      <Route path="" element={<Private />}></Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+);
