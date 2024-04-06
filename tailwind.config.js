fd6007a7ac35abca52dec10d94a059da2b77c76b
Dika Wicaksono <dhkwck00@gmail.com>
2024-04-06 16:22:06 +0700

    add some config and installing some library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+import flowbite from "flowbite-react/tailwind";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {
       fontFamily: {
@@ -9,5 +11,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
