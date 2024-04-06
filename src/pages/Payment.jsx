0a696714a5ef220f0f45473f1c0dfb80facaa3e9
Dika Wicaksono <dhkwck00@gmail.com>
2024-04-06 19:48:02 +0700

    create Payment page

---
+++ src/pages/Payment.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Card from "../components/Card";
+import Table from "../components/Table";
+import { FaSearch } from "react-icons/fa";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { IconButton, Menu, MenuItem } from "@mui/material";
+import { FilterList } from "@mui/icons-material";
+import Checkbox from "@mui/material/Checkbox";
+
+const Payment = () => {
+  return <></>;
+};
+
+export default Payment;
