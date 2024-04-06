5c6026b9f11cba015beec0ee528ad14217029a84
Dika Wicaksono <dhkwck00@gmail.com>
2024-04-06 16:41:50 +0700

    Revert "add image for login page"
    This reverts commit c825bc29804d8253681db5e399d0acef0f5fa19e.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import {
-  FaRegEye,
-  FaEyeSlash,
-  FaEnvelope,
-  FaLock,
-  FaGoogle,
-} from "react-icons/fa";
+import { FaRegEye, FaEyeSlash } from "react-icons/fa";
 import imgLogin from "../assets/img-login.jpg";
-import imgLogo from "../assets/img-logo-2.png";
 
 const Login = () => {
   const [error] = useState("");
@@ -27,50 +20,52 @@ const Login = () => {
       />
       <div className="flex items-center justify-center w-3/5">
         <form>
-          <div className="flex flex-col items-center justify-center">
-            <img src={imgLogo} alt="img-logo" className="mb-8 w-20" />
-            <h1
-              style={{ marginBottom: "70px" }}
-              className="text-2xl font-bold text-purple-700 text-center font-poppins"
+          <h1 className="text-2xl font-bold primary-text mb-5 text-center font-montserrat">
+            Login
+          </h1>
+          <div className="mb-1">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
             >
-              Login to Admin
-            </h1>
-          </div>
-          <div className="flex mb-4 flex-wrap relative rounded-2xl items-center">
-            <div className="relative w-full">
-              <span
-                className="absolute inset-y-0 left-3 flex items-center"
-                style={{ marginTop: "0.375rem", opacity: "30%" }}
-              >
-                <FaEnvelope style={{ marginBottom: "0.125rem" }} />
-              </span>
-              <input
-                type="text"
-                id="email"
-                // value={email}
-                // onChange={(e) => setEmail(e.target.value)}
-                className="flex-shrink flex-grow text-sm mt-1 w-full p-2 pl-10 border lg:w-[500px] md:w-[400px] rounded-2xl font-poppins"
-                placeholder="Email"
-                required
-              />
-            </div>
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              //   value={email}
+              //   onChange={(e) => setEmail(e.target.value)}
+              className="text-sm mt-1 w-full p-2 ps-4 border lg:w-[500px] md:w-[400px] rounded-2xl"
+              placeholder="Masukkan email"
+              required
+            />
           </div>
 
+          <div className="flex items-center mb-1 justify-between">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <Link to="/forgot-password">
+              <label
+                htmlFor="forgot-password"
+                className={`block text-xs lg:text-sm font-medium cursor-pointer text-slate-400`}
+              >
+                Lupa Kata Sandi
+              </label>
+            </Link>
+          </div>
           <div className="flex flex-wrap relative rounded-2xl items-center">
             <div className="relative w-full">
-              <span
-                className="absolute inset-y-0 left-3 flex items-center"
-                style={{ marginTop: "0.375rem" }}
-              >
-                <FaLock style={{ marginBottom: "0.125rem", opacity: "30%" }} />
-              </span>
               <input
                 type={showPassword ? "text" : "password"}
                 id="search"
                 // value={password}
                 // onChange={(e) => setPassword(e.target.value)}
-                className="flex-shrink flex-grow text-sm mt-1 w-full p-2 pl-10 border lg:w-[500px] md:w-[400px] rounded-2xl font-poppins"
-                placeholder="Password"
+                className="flex-shrink flex-grow text-sm flex-1 w-full p-2 ps-4 border rounded-2xl"
+                placeholder="Masukkan Password"
                 required
               />
               <span
@@ -81,29 +76,15 @@ const Login = () => {
               </span>
             </div>
           </div>
-
           {error && (
             <p className="text-red-500 text-sm text-center mt-2">{error}</p>
           )}
           <Link to="/dashboard">
             <button
-              className="text-white w-full font-bold text-sm h-[50px] mt-8 rounded-xl bg-purple-700 hover:bg-purple-800 transition duration-700 ease-in-out font-poppins"
+              className="text-white w-full font-bold text-sm h-[50px] mt-5 rounded-2xl bg-slate-800"
               type="submit"
             >
-              Log In
-            </button>
-          </Link>
-          <div className="flex justify-center mb-4 mt-4">
-            <p>─────</p>
-            <p className="me-2 ms-2 font-poppins">Or</p>
-            <p>─────</p>
-          </div>
-          <Link to="/dashboard">
-            <button
-              className="text-black w-full font-bold text-sm h-[50px] mt-2 rounded-xl bg-white border border-slate-900 hover:bg-slate-900 hover:text-white transition duration-700 ease-in-out flex items-center justify-center font-poppins"
-              type="submit"
-            >
-              <FaGoogle className="mr-4" /> Sign in with Google
+              Masuk
             </button>
           </Link>
         </form>
