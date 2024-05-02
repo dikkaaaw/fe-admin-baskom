2d7e24d7d35a1f2bab1e01ba7ea75a41227fe05f
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-02 16:48:37 +0700

    change routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Payment from "./pages/Payment.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Product from "./pages/Product.jsx";
+import User from "./pages/User.jsx";
+import Layout from "./components/Layout.jsx";
+import UserProfile from "./pages/UserProfile.jsx";
+import "./index.css";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    setIsAuthenticated(!!accessToken);
+  }, []);
+
+  const Private = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
+
+  const Auth = ({ element }) => {
+    return isAuthenticated ? <Navigate to="/" /> : element;
+  };
   return (
-    <>
-      <Outlet />
-    </>
+    <Router>
+      <Routes>
+        <Route path="" element={<Login />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="payment" element={<Payment />} />
+          <Route path="user" element={<User />} />
+          <Route path="product" element={<Product />} />
+          <Route path="user-profile" element={<UserProfile />} />
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store.jsx";
-import Login from "./pages/Login.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import Payment from "./pages/Payment.jsx";
-import NotFound from "./pages/NotFound.jsx";
-import Product from "./pages/Product.jsx";
-import User from "./pages/User.jsx";
-import UserProfile from "./pages/UserProfile.jsx";
-import Layout from "./components/Layout.jsx";
-import Private from "./private/private.jsx";
 import "./index.css";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="" element={<Login />} />
-      <Route path="" element={<Layout />}>
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="payment" element={<Payment />} />
-        <Route path="user" element={<User />} />
-        <Route path="product" element={<Product />} />
-        <Route path="user-profile" element={<UserProfile />} />
-      </Route>
-      <Route path="" element={<Private />}></Route>
-      <Route path="*" element={<NotFound />}></Route>
-    </Route>
-  )
-);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
