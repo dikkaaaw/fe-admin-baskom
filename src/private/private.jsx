43cfb92b37aafd85e7984ee53d5b0349e870020e
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-02 16:47:36 +0700

    delete unnecessary file

--- src/private/private.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-const Private = () => {
-  const { token } = useSelector((state) => state.auth);
-  return token ? <Outlet /> : <Navigate to="/" replace />;
-};
-
-export default Private;
