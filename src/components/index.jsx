bb53bdb2aab6123d09c25a2eaf8174854a08ea96
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-23 14:00:30 +0700

    add index file for export and aliases

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as Sidebar } from "./sidebar/sidebar";
+export { default as AreaCards } from "./dashboard/areaCards/AreaCards";
+export { default as AreaTable } from "./dashboard/areaTable/AreaTable";
+export { default as AreaTop } from "./dashboard/areaTop/AreaTop";
---
+++ src/screens/index.jsx
@@ -0,0 +1,6 @@
+export { default as Login } from "./auth/login/LoginScreen";
+export { default as Dashboard } from "./dashboard/DashboardScreen";
+export { default as User } from "./user/UserScreen";
+export { default as Transaction } from "./transaction/TransactionScreen";
+export { default as Product } from "./product/ProductScreen";
+export { default as PageNotFound } from "./error/PageNotFound";
