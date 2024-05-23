610ee3ad501fe0dbc8a54727de44cd6a4f00c046
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-23 14:02:49 +0700

    add new product file

---
+++ src/screens/product/ProductScreen.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { AreaCards, AreaTable, AreaTop } from "../../components";
+import "../loading.scss";
+
+const Product = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <div className="spinner"></div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="content-area">
+      <AreaTop />
+      <AreaCards />
+      <AreaTable />
+    </div>
+  );
+};
+
+export default Product;
