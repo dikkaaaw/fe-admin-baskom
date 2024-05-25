ef79560e0060fb6a2821ec97ae60bdbf041dd2b4
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-25 15:25:07 +0700

    export new new aliases

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -3,4 +3,5 @@ export { default as Dashboard } from "./dashboard/DashboardScreen";
 export { default as User } from "./user/UserScreen";
 export { default as Transaction } from "./transaction/TransactionScreen";
 export { default as Product } from "./product/ProductScreen";
+export { default as UserSetting } from "./settings/UserSettingScreen";
 export { default as PageNotFound } from "./error/PageNotFound";
