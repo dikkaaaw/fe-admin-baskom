c0935b07d52623c63264509f4e4cdfae2ca1666a
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-28 01:26:34 +0700

    add aliases for user settings

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,3 +3,4 @@ export { default as AreaCards } from "./dashboard/areaCards/AreaCards";
 export { default as AreaTable } from "./dashboard/areaTable/AreaTable";
 export { default as AreaTop } from "./dashboard/areaTop/AreaTop";
 export { default as SearchBar } from "./dashboard/searchBar/SearchBar";
+export { default as UserSettings } from "./dashboard/userSettings/UserSettings";
