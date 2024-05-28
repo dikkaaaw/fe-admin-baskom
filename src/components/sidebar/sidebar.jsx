cf7b9659f6da03be822e08cb5ba98599f265a493
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-29 03:30:11 +0700

    add modal for logout handle and doing some refactor

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -9,20 +9,20 @@ import {
 } from "react-icons/md";
 import { Link, useLocation } from "react-router-dom";
 import { SidebarContext } from "../../context/SidebarContext";
+import LogoutAction from "../dashboard/logoutModal/logoutAction";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
-  //   const { theme } = useContext(ThemeContext);
   const { isSidebarOpen, closeSidebar } = useContext(SidebarContext);
   const navbarRef = useRef(null);
   const location = useLocation();
   const [activeItem, setActiveItem] = useState(location.pathname);
+  const [showLogoutModal, setShowLogoutModal] = useState(false); // State to control modal visibility
 
   useEffect(() => {
     setActiveItem(location.pathname);
   }, [location.pathname]);
 
-  // closing the navbar when clicked outside the sidebar area
   const handleClickOutside = (event) => {
     if (
       navbarRef.current &&
@@ -40,82 +40,101 @@ const Sidebar = () => {
     };
   }, []);
 
-  return (
-    <nav
-      className={`sidebar ${isSidebarOpen ? "sidebar-show" : ""}`}
-      ref={navbarRef}
-    >
-      <div className="sidebar-top">
-        <div className="sidebar-brand">
-          {/* <img src={theme === LIGHT_THEME ? LogoBlue : LogoWhite} alt="" /> */}
-          <Link to="/dashboard">
-            <span className="sidebar-brand-text">BasKom.</span>
-          </Link>
-        </div>
-        <button className="sidebar-close-btn" onClick={closeSidebar}>
-          <MdOutlineClose size={24} />
-        </button>
-      </div>
-      <div className="sidebar-body">
-        <div className="sidebar-menu">
-          <ul className="menu-list">
-            <li
-              className={`menu-link ${activeItem === "/dashboard" ? "active" : ""}`}
-            >
-              <Link to="/dashboard" className="menu-link">
-                <span className="menu-link-icon">
-                  <MdOutlineGridView size={18} />
-                </span>
-                <span className="menu-link-text">Dashboard</span>
-              </Link>
-            </li>
-            <li
-              className={`menu-link ${activeItem === "/users" ? "active" : ""}`}
-            >
-              <Link to="/users" className="menu-link">
-                <span className="menu-link-icon">
-                  <MdOutlinePeople size={20} />
-                </span>
-                <span className="menu-link-text">Kelola User</span>
-              </Link>
-            </li>
-            <li
-              className={`menu-link ${activeItem === "/transactions" ? "active" : ""}`}
-            >
-              <Link to="/transactions" className="menu-link">
-                <span className="menu-link-icon">
-                  <MdOutlineCurrencyExchange size={18} />
-                </span>
-                <span className="menu-link-text">Kelola Transaksi</span>
-              </Link>
-            </li>
-          </ul>
-        </div>
+  const handleLogoutClick = () => {
+    setShowLogoutModal(true);
+    closeSidebar();
+  };
 
-        <div className="sidebar-menu sidebar-menu2">
-          <ul className="menu-list">
-            <li
-              className={`menu-link ${activeItem === "/user-settings" ? "active" : ""}`}
-            >
-              <Link to="/user-settings" className="menu-link">
-                <span className="menu-link-icon">
-                  <MdOutlineSettings size={20} />
-                </span>
-                <span className="menu-link-text">Settings</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link to="/" className="menu-link">
-                <span className="menu-link-icon">
-                  <MdOutlineLogout size={20} />
-                </span>
-                <span className="menu-link-text">Logout</span>
-              </Link>
-            </li>
-          </ul>
+  const handleCloseModal = () => {
+    setShowLogoutModal(false);
+  };
+
+  return (
+    <>
+      <nav
+        className={`sidebar ${isSidebarOpen ? "sidebar-show" : ""}`}
+        ref={navbarRef}
+      >
+        <div className="sidebar-top">
+          <div className="sidebar-brand">
+            <Link to="/dashboard">
+              <span className="sidebar-brand-text">BasKom.</span>
+            </Link>
+          </div>
+          <button className="sidebar-close-btn" onClick={closeSidebar}>
+            <MdOutlineClose size={24} />
+          </button>
         </div>
-      </div>
-    </nav>
+        <div className="sidebar-body">
+          <div className="sidebar-menu">
+            <ul className="menu-list">
+              <li
+                className={`menu-link ${
+                  activeItem === "/dashboard" ? "active" : ""
+                }`}
+              >
+                <Link to="/dashboard" className="menu-link">
+                  <span className="menu-link-icon">
+                    <MdOutlineGridView size={18} />
+                  </span>
+                  <span className="menu-link-text">Dashboard</span>
+                </Link>
+              </li>
+              <li
+                className={`menu-link ${
+                  activeItem === "/users" ? "active" : ""
+                }`}
+              >
+                <Link to="/users" className="menu-link">
+                  <span className="menu-link-icon">
+                    <MdOutlinePeople size={20} />
+                  </span>
+                  <span className="menu-link-text">Kelola User</span>
+                </Link>
+              </li>
+              <li
+                className={`menu-link ${
+                  activeItem === "/transactions" ? "active" : ""
+                }`}
+              >
+                <Link to="/transactions" className="menu-link">
+                  <span className="menu-link-icon">
+                    <MdOutlineCurrencyExchange size={18} />
+                  </span>
+                  <span className="menu-link-text">Kelola Transaksi</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="sidebar-menu sidebar-menu2">
+            <ul className="menu-list">
+              <li
+                className={`menu-link ${
+                  activeItem === "/user-settings" ? "active" : ""
+                }`}
+              >
+                <Link to="/user-settings" className="menu-link">
+                  <span className="menu-link-icon">
+                    <MdOutlineSettings size={20} />
+                  </span>
+                  <span className="menu-link-text">Settings</span>
+                </Link>
+              </li>
+              <li className={`menu-link ${activeItem === "/" ? "active" : ""}`}>
+                <a className="menu-link" onClick={handleLogoutClick}>
+                  <span className="menu-link-icon">
+                    <MdOutlineLogout size={20} />
+                  </span>
+                  <span className="menu-link-text">Logout</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <LogoutAction show={showLogoutModal} onClose={handleCloseModal} />
+    </>
   );
 };
 
