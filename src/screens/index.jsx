398820ec671be70790c6030f0e016797389eb84a
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-30 17:09:17 +0700

    remove user setting page from aliases

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -2,5 +2,4 @@ export { default as Login } from "./auth/login/LoginScreen";
 export { default as Dashboard } from "./dashboard/DashboardScreen";
 export { default as User } from "./user/UserScreen";
 export { default as Transaction } from "./transaction/TransactionScreen";
-export { default as UserSetting } from "./settings/UserSettingScreen";
 export { default as PageNotFound } from "./error/PageNotFound";
