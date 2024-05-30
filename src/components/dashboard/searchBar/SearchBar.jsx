c0d6821b337b958cf62f782c68bf89b3e939888a
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-30 17:08:32 +0700

    refactor code

--- src/components/dashboard/searchBar/SearchBar.jsx
+++ src/components/dashboard/searchBar/SearchBar.jsx
@@ -1,40 +1,13 @@
-import { useState } from "react";
-import { MdOutlineSearch } from "react-icons/md";
 import { FaFilter } from "react-icons/fa";
 import "./searchBar.scss";
 
-const SearchBar = ({ onSearch, onFilter }) => {
-  const [query, setQuery] = useState("");
-
-  const handleInputChange = (e) => {
-    setQuery(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (onSearch) {
-      onSearch(query);
-    }
-  };
-
-  const handleFilterClick = () => {
-    if (onFilter) {
-      onFilter();
-    }
-  };
-
+const SearchBar = () => {
   return (
-    <form className="search-bar" onSubmit={handleSubmit}>
-      <div className="filter-box" onClick={handleFilterClick}>
+    <form className="search-bar">
+      <div className="filter-box">
         <FaFilter className="filter-icon" />
       </div>
-      <input
-        type="text"
-        placeholder="Search..."
-        value={query}
-        onChange={handleInputChange}
-        className="search-input"
-      />
+      <input type="text" placeholder="Search..." className="search-input" />
       <button type="submit" className="search-button">
         Search
       </button>
