2cef2b8c0bbb3ae32f15eb6b79d7f91e941c62d2
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-30 17:07:00 +0700

    delete user setting aliases

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,5 +3,4 @@ export { default as AreaCards } from "./dashboard/areaCards/AreaCards";
 export { default as AreaTable } from "./dashboard/areaTable/AreaTable";
 export { default as AreaTop } from "./dashboard/areaTop/AreaTop";
 export { default as SearchBar } from "./dashboard/searchBar/SearchBar";
-export { default as UserSettings } from "./dashboard/userSettings/UserSettingsTop";
-export { default as UserSettingsForm } from "./dashboard/userSettings/UserSettingsForm";
+export { default as UserProfileModal } from "./dashboard/userProfileModal/UserProfileModal";
