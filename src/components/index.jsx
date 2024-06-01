60a5492412abcd41dca4d523a044c7f29463abf6
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-01 17:31:15 +0700

    add new aliases for table upgrade

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,7 @@
 export { default as Sidebar } from "./sidebar/sidebar";
 export { default as AreaCards } from "./dashboard/areaCards/AreaCards";
 export { default as AreaTable } from "./dashboard/areaTable/AreaTable";
+export { default as AreaTableUpgrade } from "./dashboard/areaTableUpgrade/AreaTableUpgrade";
 export { default as AreaTop } from "./dashboard/areaTop/AreaTop";
 export { default as SearchBar } from "./dashboard/searchBar/SearchBar";
 export { default as UserProfileModal } from "./dashboard/userProfileModal/UserProfileModal";
