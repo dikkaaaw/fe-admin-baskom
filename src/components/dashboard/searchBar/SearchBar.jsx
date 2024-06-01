751c57762800c0d602a173d682a1bac85fbf11df
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-01 17:33:52 +0700

    add back search bar component

--- src/components/dashboard/searchBar/SearchBar.jsx
+++ src/components/dashboard/searchBar/SearchBar.jsx
@@ -1,18 +1,27 @@
+import { PropTypes } from "prop-types";
 import { FaFilter } from "react-icons/fa";
 import "./searchBar.scss";
 
-const SearchBar = () => {
+const SearchBar = ({ searchQuery, setSearchQuery }) => {
   return (
-    <form className="search-bar">
+    <form className="search-bar" onSubmit={(e) => e.preventDefault()}>
       <div className="filter-box">
         <FaFilter className="filter-icon" />
       </div>
-      <input type="text" placeholder="Search..." className="search-input" />
-      <button type="submit" className="search-button">
-        Search
-      </button>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search-input"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
     </form>
   );
+};
+
+SearchBar.propTypes = {
+  searchQuery: PropTypes.string.isRequired,
+  setSearchQuery: PropTypes.string.isRequired,
 };
 
 export default SearchBar;
