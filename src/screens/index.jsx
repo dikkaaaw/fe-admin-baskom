c467641094f09ae9945bec9d942a5a6c2c10f8d9
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-01 17:36:50 +0700

    changing some aliases

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,5 +1,5 @@
 export { default as Login } from "./auth/login/LoginScreen";
 export { default as Dashboard } from "./dashboard/DashboardScreen";
 export { default as User } from "./user/UserScreen";
-export { default as Transaction } from "./transaction/TransactionScreen";
+export { default as RoleUpgrade } from "./roleUpgrade/RoleUpgradeScreen";
 export { default as PageNotFound } from "./error/PageNotFound";
