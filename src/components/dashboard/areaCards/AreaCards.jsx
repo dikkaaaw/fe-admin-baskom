952d8752ec9403c35d2f26d3463a7332575310c6
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-22 00:45:53 +0700

    feat: Add productPosted and userGrowthPercentage states to AreaCards component
    This commit adds the productPosted and userGrowthPercentage states to the AreaCards component. The productPosted state is used to track the number of products posted by users, fetched from the API endpoint "https://baskom-api.up.railway.app/api/v1/products". The userGrowthPercentage state calculates the growth percentage of active users compared to the previous count. These states are used to display the growth of products and users in the AreaCards component.

--- src/components/dashboard/areaCards/AreaCards.jsx
+++ src/components/dashboard/areaCards/AreaCards.jsx
@@ -5,6 +5,11 @@ import "./AreaCards.scss";
 
 const AreaCards = () => {
   const [activeUserCount, setActiveUserCount] = useState(0);
+  const [productPosted, setProductPosted] = useState(0);
+  const [previousProductPosted] = useState(9);
+  const [previousActiveUserCount] = useState(8);
+  const [userGrowthPercentage, setUserGrowthPercentage] = useState(0);
+  const [productGrowthPercentage, setProductGrowthPercentage] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,6 +31,41 @@ const AreaCards = () => {
 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    const fetchDataProduct = async () => {
+      const token = localStorage.getItem("token");
+      try {
+        const response = await axios.get(
+          "https://baskom-api.up.railway.app/api/v1/products",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setProductPosted(response.data.length);
+      } catch (error) {
+        console.error("Error fetching user data: ", error);
+      }
+    };
+
+    fetchDataProduct();
+  }, []);
+
+  useEffect(() => {
+    const growth =
+      ((activeUserCount - previousActiveUserCount) / previousActiveUserCount) *
+      100;
+    setUserGrowthPercentage(isNaN(growth) ? 0 : growth);
+  }, [activeUserCount, previousActiveUserCount]);
+
+  useEffect(() => {
+    const productGrowth =
+      ((productPosted - previousProductPosted) / previousProductPosted) * 100;
+    setProductGrowthPercentage(isNaN(productGrowth) ? 0 : productGrowth);
+  }, [productPosted, previousProductPosted]);
+
   return (
     <section className="content-area-cards">
       <AreaCard
@@ -39,20 +79,20 @@ const AreaCards = () => {
       />
       <AreaCard
         colors={["#e4e8ef", "#4ce13f"]}
-        percentFillValue={20}
+        percentFillValue={userGrowthPercentage}
         cardInfo={{
-          title: "Todays Revenue",
-          value: "IDR 200.000",
-          text: "Available to payout",
+          title: "User Growth",
+          value: `${userGrowthPercentage.toFixed(2)}%`,
+          text: "User registered since last month",
         }}
       />
       <AreaCard
         colors={["#e4e8ef", "#f29a2e"]}
-        percentFillValue={25}
+        percentFillValue={productGrowthPercentage}
         cardInfo={{
-          title: "Total Revenue",
-          value: "IDR 2.500.000",
-          text: "Available to payout",
+          title: "Product Growth",
+          value: `${productGrowthPercentage.toFixed(2)}%`,
+          text: "Total product posted by users since last month",
         }}
       />
     </section>
