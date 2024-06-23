f028dcb7a1e121235390701a85495556450bdf4f
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-23 18:18:40 +0700

    refactor: Add toast notification for successful logout

--- src/components/dashboard/logoutModal/logoutAction.jsx
+++ src/components/dashboard/logoutModal/logoutAction.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { FaExclamationTriangle } from "react-icons/fa";
 import "./logoutAction.scss";
 
@@ -7,13 +9,23 @@ const LogoutAction = ({ show, onClose }) => {
   const navigate = useNavigate();
 
   const onLogout = () => {
-    navigate("/");
+    toast.success("Logout successful!", {
+      closeOnClick: true,
+      hideProgressBar: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+    });
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
   };
 
   return (
     <>
       {show && (
         <div className="modal-overlay">
+          <ToastContainer />
           <div className="modal">
             <div className="alert-icon">
               <FaExclamationTriangle size={40} />
